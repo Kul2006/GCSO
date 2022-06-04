6bd07484ecbf081614fba502effa5f77cdf05e7b
Kul2006 <89687688+Kul2006@users.noreply.github.com>
2022-06-04 10:53:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup() {
     background(0); 
     if(wall.x - car.x < (car.width + wall.width)/2){ car.velocityX = 0; 
       
-      var deformation = (0.5 * wieght * speed * speed)/22500; 
+      var deformation = (0.5 * weight * speed * speed)/22500; 
       
       if(deformation < 80){ 
         car.shapeColor = "green"; 
